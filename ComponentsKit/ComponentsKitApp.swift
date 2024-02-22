59e29a88389753060b783ebbd6b4ef86592889fe
Uliana Gritchina <ugritchina@icloud.com>
2024-02-22 11:43:02 +0300

    Initial Commit

---
+++ ComponentsKit/ComponentsKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComponentsKitApp.swift
+//  ComponentsKit
+//
+//  Created by Ульяна Гритчина on 22.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct ComponentsKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
